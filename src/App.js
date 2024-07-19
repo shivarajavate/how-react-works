f3831f5b4782868103549565d1d76dfd4e8b30b3
Shivaraj Avate <shivaraj.avate@mantle3d.com>
2024-07-19 23:26:02 +0530

    feat: include necessary actions

--- src/App.js
+++ src/App.js
@@ -63,7 +63,20 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    setLikes((likes) => likes + 3);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoIn2seconds() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -79,13 +92,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoIn2seconds}>Undo in 2s</button>
       </div>
     </div>
   );
